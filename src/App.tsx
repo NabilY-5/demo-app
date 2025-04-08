281a247726749a0d5e2b2e94c14f67e682d2c512
sunnysmilen <davidsalnicole@gmail.com>
2025-04-08 09:56:41 +0200

    created button and add CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,44 +3,49 @@ import Cardlist from "./components/Cardlist";
 import "./App.css";
 
 const countriesList = [
-	{
-		name: "Paris",
-		image: "Paris.avif",
-		description:
-			"Paris, la capitale de la France, est un véritable musée à ciel ouvert où l'histoire, l'art et la culture se mêlent harmonieusement. Réputée pour ses monuments emblématiques, la ville vous invite à découvrir la majestueuse Tour Eiffel, qui offre une vue panoramique imprenable sur la ville, ou encore la Cathédrale Notre-Dame, chef-d'œuvre de l'architecture gothique.",
-		// Heure local : CEST
-	},
-	{
-		name: "Londres",
-		image: "Londres.jpg",
-		description:
-			"Londres, capitale dynamique du Royaume-Uni, est une ville qui allie tradition et modernité avec une étonnante aisance. Les visiteurs peuvent commencer leur exploration par les sites iconiques comme le Palais de Buckingham, résidence de la Reine, ou l'abbaye de Westminster, témoin de siècles d'histoire royale. La fameuse horloge Big Ben et le Parlement britannique, majestueux au bord de la Tamise, sont également des incontournables.",
-		// Heure local : "GMT+1"
-	},
-	{
-		name: "Tokyo",
-		image: "Tokyo.jpg",
-		description:
-			"Tokyo,a capitale du Japon, est une métropole vibrante où tradition et innovation se rencontrent à chaque coin de rue. C’est une ville où l’ancien et le moderne coexistent parfaitement : entre les temples anciens comme le Senso-ji à Asakusa et les quartiers ultramodernes comme Shibuya et Shinjuku, Tokyo offre une expérience unique.",
-		// Heure local :GMT +9
-	},
-	{
-		name: "New York",
-		image: "New-York.avif",
-		description:
-			"New York, la ville qui ne dort jamais, est un véritable carrefour de cultures, d'art et de possibilités infinies. En tant que l'une des plus grandes métropoles du monde, elle offre une diversité étonnante et une énergie palpable à chaque coin de rue.",
-		// Heure local : GMT -4
-	},
+  {
+    name: "Paris",
+    image: "Paris.avif",
+    description:
+      "Paris, la capitale de la France, est un véritable musée à ciel ouvert où l'histoire, l'art et la culture se mêlent harmonieusement. Réputée pour ses monuments emblématiques, la ville vous invite à découvrir la majestueuse Tour Eiffel, qui offre une vue panoramique imprenable sur la ville, ou encore la Cathédrale Notre-Dame, chef-d'œuvre de l'architecture gothique.",
+    // Heure local : CEST
+  },
+  {
+    name: "Londres",
+    image: "Londres.jpg",
+    description:
+      "Londres, capitale dynamique du Royaume-Uni, est une ville qui allie tradition et modernité avec une étonnante aisance. Les visiteurs peuvent commencer leur exploration par les sites iconiques comme le Palais de Buckingham, résidence de la Reine, ou l'abbaye de Westminster, témoin de siècles d'histoire royale. La fameuse horloge Big Ben et le Parlement britannique, majestueux au bord de la Tamise, sont également des incontournables.",
+    // Heure local : "GMT+1"
+  },
+  {
+    name: "Tokyo",
+    image: "Tokyo.jpg",
+    description:
+      "Tokyo,a capitale du Japon, est une métropole vibrante où tradition et innovation se rencontrent à chaque coin de rue. C’est une ville où l’ancien et le moderne coexistent parfaitement : entre les temples anciens comme le Senso-ji à Asakusa et les quartiers ultramodernes comme Shibuya et Shinjuku, Tokyo offre une expérience unique.",
+    // Heure local :GMT +9
+  },
+  {
+    name: "New York",
+    image: "New-York.avif",
+    description:
+      "New York, la ville qui ne dort jamais, est un véritable carrefour de cultures, d'art et de possibilités infinies. En tant que l'une des plus grandes métropoles du monde, elle offre une diversité étonnante et une énergie palpable à chaque coin de rue.",
+    // Heure local : GMT -4
+  },
 ];
 
 function App() {
-	return (
-		<div>
-			<Navbar />
-			<h1>Mon blog voyage</h1>
-			<Cardlist countries={countriesList} />
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <h1>Mon blog voyage</h1>
+      <img
+        src="avion trajet.jpg"
+        alt="Trajet poitillé d'un avion"
+        className="Airplane"
+      />
+      <Cardlist countries={countriesList} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Cardlist.tsx
+++ src/components/Cardlist.tsx
@@ -1,26 +1,26 @@
 import Card from "./Card";
 
 type CountryType = {
-	name: string;
-	image: string;
-	description: string;
+  name: string;
+  image: string;
+  description: string;
 };
 
 function Cardlist({ countries }: { countries: CountryType[] }) {
-	return (
-		<>
-			<div className="cardlist">
-				{countries.map((country) => (
-					<Card
-						key={country.name}
-						name={country.name}
-						image={country.image}
-						description={country.description}
-					/>
-				))}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="cardlist">
+        {countries.map((country) => (
+          <Card
+            key={country.name}
+            name={country.name}
+            image={country.image}
+            description={country.description}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Cardlist;

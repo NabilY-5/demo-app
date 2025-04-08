1da9f54abe7cc958a9152a3597833c0bfbeb3a8f
NabilY-5 <nabilyahya59280@gmail.com>
2025-04-08 12:01:57 +0200

    Work on the project

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,19 @@
 type CountryType = {
-  name: string;
-  image: string;
-  description: string;
+	name: string;
+	image: string;
+	description: string;
 };
 
 function Card({ name, image, description }: CountryType) {
-  const handleclick = () => {};
-  return (
-    <>
-      <span>
-        <button onClick={() => handleclick} className="Button">
-          {name}
-        </button>
-      </span>
-      <div className="card">
-        <h2 className="card-title">{name}</h2>
-        <p className="card-description">{description}</p>
-        <img src={image} alt={name} className="card-image" />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="card">
+				<h2 className="card-title">{name}</h2>
+				<p className="card-description">{description}</p>
+				<img src={image} alt={name} className="card-image" />
+			</div>
+		</>
+	);
 }
 
 export default Card;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+	return (
+		<div className="footer">
+			<h3>Made by Florentin, Nabil et Nicole</h3>
+		</div>
+	);
+}
+
+export default Footer;
